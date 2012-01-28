9bdc3a8ccafa79854de527c835aa663ab6503d30
Jason Wong <technique@gmail.com>
2012-01-28 09:20:18 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Ruby on Rails Tutorial: sample application
+
+This is the sample application for [*RoR Tutorial: Learn Rails by Example*](http://railstutorial.org/) by [Micahel Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '6cb7eb65c84531930ffd3098c0d7e1798c1fa02542a11f4ca471ad744fa6e5245aa53cf2ffaaa3058fb751c1c809248c0f4ed430b97b5689e52fcad970596687'
